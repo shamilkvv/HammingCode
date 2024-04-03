b4fd00d6031d43057e7000bdd80fd518d9c594a7
shamilkvv <151441020+shamilkvv@users.noreply.github.com>
2024-04-04 00:07:22 +0400

    Update coder.cpp

--- coder.cpp
+++ coder.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <bitset>
 
-class HammingEncoder {
-private:
-    std::bitset<8> EncodePart(const std::bitset<4>& part) {
+class HammingCoder {
+public:
+    static std::bitset<8> encode(std::bitset<4> &part) {
         std::bitset<8> encoded;
 
         bool d1 = part[0];
@@ -28,35 +28,95 @@ private:
         return encoded;
     }
 
-public:
-    std::bitset<16> Encode(char sym) {
-        std::bitset<8> bits(sym);
-        std::bitset<16> encoded;
+    static std::bitset<4> decode(std::bitset<8> encoded) {
+        std::bitset<4> decoded;
 
-        std::bitset<4> first(bits.to_string().substr(0, 4));
-        std::bitset<4> second(bits.to_string().substr(4, 4));
+        bool p1 = encoded[0];
+        bool d1 = encoded[1];
+        bool p2 = encoded[2];
+        bool d2 = encoded[3];
+        bool p3 = encoded[4];
+        bool d3 = encoded[5];
+        bool p4 = encoded[6];
+        bool d4 = encoded[7];
 
-        std::bitset<8> encodedFirstPart = EncodePart(first);
-        std::bitset<8> encodedSecondPart = EncodePart(second);
+        bool A = (p1 + d1 + d3 + d4) % 2;
+        bool B = (d1 + p2 + d2 + d4) % 2;
+        bool C = (d1 + d2 + p3 + d3) % 2;
+        bool D = (p1 + d1 + p2 + d2 + p3 + d3 + p4 + d4) % 2;
 
-        encoded |= (encodedFirstPart.to_ulong() << 8);
-        encoded |= encodedSecondPart.to_ulong();
+        if ((A == 0 || B == 0 || C == 0) && D == 1) {
+            std::cout << "Error detected, cannot be corrected." << std::endl;
+            return decoded;
+        }
 
-        return encoded;
+        if ((A == 0 || B == 0 || C == 0) && D == 0) {
+            if (A == 0 && B == 0 && C == 0) {
+                d1 ^= 1;
+            }
+            if (A == 1 && B == 0 && C == 0) {
+                d2 ^= 1;
+            }
+            if (A == 0 && B == 1 && C == 0) {
+                d3 ^= 1;
+            }
+            if (A == 0 && B == 0 && C == 1) {
+                d4 ^= 1;
+            }
+        }
+
+        decoded[0] = d1;
+        decoded[1] = d2;
+        decoded[2] = d3;
+        decoded[3] = d4;
+
+        return decoded;
     }
-
 };
+
+int Encode1(char sym)
+{
+    std::bitset<8> bits(sym);
+    std::bitset<16> encoded;
+
+    std::bitset<4> first(bits.to_string().substr(0, 4));
+    std::bitset<4> second(bits.to_string().substr(4, 4));
+
+    std::bitset<8> encodedFirstPart = HammingCoder::encode(first);
+    std::bitset<8> encodedSecondPart = HammingCoder::encode(second);
+
+    encoded |= (encodedFirstPart.to_ulong() << 8);
+
+    encoded |= encodedSecondPart.to_ulong();
+
+    int value = static_cast<int>(encoded.to_ulong());
+
+    return value;
+}
+
+
+
+int decode1(short c)
+{
+    int g = c >> 8;
+    int t = c;
+    int H = (HammingCoder::decode(std::bitset<8>(g)).to_ulong() << 4) + HammingCoder::decode(std::bitset<8>(t)).to_ulong();
+
+
+    return H;
+}
 
 int main() {
     char sym;
     std::cin >> sym;
 
-    HammingEncoder encoder;
-    std::bitset<8> tmp(sym);
-    std::bitset<16> encodedSym = encoder.Encode(sym);
+    int encodedSym = Encode1(sym);
 
-    std::cout << "Original: " << sym << " = " << tmp << '\n';
-    std::cout << "Encoded: " << encodedSym << '\n';
+    std::cout << "Encoded: " << std::bitset<16>(encodedSym) << std::endl;
+
+    int decodedSym = decode1(encodedSym);
+
+    std::cout << "Decoded: " << decodedSym << std::endl;
 
     return 0;
 }
